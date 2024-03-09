4394eac22e12756b0b6f415d67798b4df29744c4
Aniruddha Roy <65581823+ani3321r@users.noreply.github.com>
2024-03-09 22:30:42 +0530

    navbar

---
+++ a_simple_project/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Products">
+          <div className="flex flex-col space-v-4 text-sm">
+            <HoveredLink href="/Products">All Products</HoveredLink>
+            <HoveredLink href="/Products">canerra rs</HoveredLink>
+            <HoveredLink href="/Products">911 gt3 rs</HoveredLink>
+            <HoveredLink href="/Products">911 turbo</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
